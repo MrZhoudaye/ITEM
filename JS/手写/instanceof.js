1649efaf8cc8149ac6440cb562810d3e9e8c4b6a
MrZhoudaye <1938024999@qq.com>
2021-02-26 14:12:27 +0800

    时间格式化

--- JS/手写/instanceof.js
+++ JS/手写/instanceof.js
@@ -1,18 +1,18 @@
 // 手动实现instanceof
-function myInstanceof(aims,type){
+function myInstanceof(aims, type) {
   // 如果是基本数据类型
-  if(typeof aims !== 'object' || typeof aims === null) return false
+  if (typeof aims !== 'object' || typeof aims === null) return false
 
   // 如果是复杂数据类型
   let proto = Object.getPrototypeOf(aims)
-  console.log(proto,"-- INSERT --")
-  if(proto == null) return false
+  console.log(proto, "-- INSERT --")
+  if (proto == null) return false
 
-  if(proto == type.prototype) return true
+  if (proto == type.prototype) return true
   myInstanceof(proto)
 }
 
-console.log(myInstanceof('111',String))
-console.log(myInstanceof(new String("111"),String))
+console.log(myInstanceof('111', String))
+console.log(myInstanceof(new String("111"), String))
 
 // console.log(![],!{})
